b7737319b65836d3c16531023ca0c95e02935f36
kevinboogaard <kevin.boogaard3@gmail.com>
2015-10-12 09:07:18 +0200

    fix PLZ

---
+++ Test/src/Methods/GameObject.java
@@ -0,0 +1,17 @@
+package Methods;
+
+import Mathf.Vector2;
+
+public class GameObject {
+	public Vector2 position;
+	public Vector2 rotation;
+	public Vector2 scale;
+	public void visible(object)
+	{
+		object.setVisible(false);
+	}
+	public void destroy()
+	{
+		
+	}
+}
